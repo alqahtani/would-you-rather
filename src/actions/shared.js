7eaa256fd59655675c2c26969461557b9035ab2d
Ahmed AlQahtani <alq.ahmad@gmail.com>
2018-08-03 19:38:32 +0300

    added get initial data actions

---
+++ src/actions/shared.js
@@ -0,0 +1,14 @@
+import { _getQuestions, _getUsers } from '../utils/_DATA'
+
+import { receiveUsers } from '../actions/users'
+import { receiveQuestions } from '../actions/questions'
+import { setAuthedUser } from '../actions/authedUser'
+
+export function handleInitialData() {
+  return dispatch =>
+    Promise.all([_getQuestions(), _getUsers()]).then(([questions, users]) => {
+      dispatch(receiveQuestions(questions))
+      dispatch(receiveUsers(users))
+      dispatch(setAuthedUser(null))
+    })
+}

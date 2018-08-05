07d0731fddc3b44f65ac2aadc2c31dd43e904c4a
Ahmed AlQahtani <alq.ahmad@gmail.com>
2018-08-05 13:42:38 +0300

    fix: added answer to users portion of the state when submit answer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,5 @@
 import { RECEIVE_USERS } from '../actions/users'
-import { ADD_QUESTION } from '../actions/questions'
+import { ADD_QUESTION, ADD_ANSWER } from '../actions/questions'
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -14,6 +14,17 @@ export default function users(state = {}, action) {
         [action.question.author]: {
           ...state[action.question.author],
           questions: state[action.question.author].questions.concat(action.question.id),
+        }
+      }
+    case ADD_ANSWER:
+      return {
+        ...state,
+        [action.answer.authedUser]: {
+          ...state[action.answer.authedUser],
+          answers: {
+            ...state[action.answer.authedUser].answers,
+            [action.answer.qid]: action.answer.answer
+          }
         }
       }
     default:
